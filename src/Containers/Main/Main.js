fed896d96f82e9b183134fbebd13358f98d2d590
Nathan <nathanarohde@gmail.com>
2018-12-31 16:35:55 -0800

    Project summary JSON objects implemented.

--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import project6 from '../../data/projects/project6.json';
 
 class Main extends Component {
   render () {
     return (
       <main>
         Portfolio goes here
+        <div>
+          { project6.title }
+        </div>
       </main>
     )
   }
--- src/Containers/Sidebar/Sidebar.js
+++ src/Containers/Sidebar/Sidebar.js
@@ -5,28 +5,6 @@ import SkillGroup from '../../Components/Sidebar/SkillGroup/SkillGroup'
 class Sidebar extends Component {
 
   render () {
-    // const listSkills = Object.keys( skillGroups )
-    //                           .map( skill => {
-    //                             return skillGroups[skill]
-    //                           }).forEach(function (skillGroup) {
-    //   console.log( skillGroup );
-    //   skillGroup.forEach(function(skills){
-    //     console.log(skills);
-    //     Object.keys( skills )
-    //           .map( skill => {
-    //             console.log(skill)
-    //             console.log(skills[skill].years)
-    //             if ( skills[skill].subskills !== false ){
-    //               console.log(skills[skill].subskills)
-    //               skills[skill].subskills.forEach(function(subskill){
-    //                 console.log(subskill[0])
-    //                 console.log(subskill[1])
-    //               })
-    //             }
-    //           })
-    //   });
-    // });
-
     const skillList = Object.keys( skillGroups )
                             .map( skillGroup => {
                               return <SkillGroup key={skillGroup} skillGroups={skillGroups} skillGroup={skillGroup}></SkillGroup>
